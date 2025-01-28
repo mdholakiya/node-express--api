365f24ed6f0e109708c399db111f21e695b5a629
mdholakiya <mansidholakiya28@gmail.com>
2025-01-28 12:14:50 +0530

    live db url add

--- src/config/db.js
+++ src/config/db.js
@@ -18,14 +18,27 @@ import dotEnv from "./enviroment.js";
   //     port:dotEnv.P_PORT
   // });
   // console.log(process.env,"env--------------");
+
   
+//local db
+
+// const db = new pg.Client({
+//     user:dotEnv.USER,
+//     host:dotEnv.HOST,
+//     database:dotEnv.DATABASE,
+//     password:dotEnv.PASSWORD,
+//     port:dotEnv.P_PORT
+// });
+
+
+//live db
 const db = new pg.Client({
-    user:dotEnv.USER,
-    host:dotEnv.HOST,
-    database:dotEnv.DATABASE,
-    password:dotEnv.PASSWORD,
-    port:dotEnv.P_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
+
 
 db.connect(err => {
   if(err){
